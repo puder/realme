e80b9311352cec8b87963aae52b03a296d3967e7
XxIntensionxX <intension1992@icloud.com>
2016-04-13 09:16:51 +0200

    update

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -16,11 +16,11 @@
  */
 
 // This is where scripts' loading functions should be declared:
-
+void AddSC_welcomeOnFirstLogin();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddCustomScripts()
 {
-void AddSC_welcomeOnFirstLogin();
+	AddSC_welcomeOnFirstLogin();
 }
